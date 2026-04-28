45352133ee05c72e0e862179ee2aaa4bf74eeb3b
Pratik Srivastava <pratiksrivastava028@gmail.com>
2026-04-28 12:48:20 +0530

    Done

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,7 @@ app.use(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://order-karo-xkoe.onrender.com",
         FRONTEND_URL,
       ];
       if (
